d0946199214ab889140b2952779a16e6878b20e4
Miro Kurka <59745102+miroslavkurka@users.noreply.github.com>
2022-10-26 19:52:40 +0200

    first hw completed

--- first_task.m
+++ first_task.m
@@ -1,13 +1,68 @@
-mat1=randn(4,5)
-mat2=randn(4,5)
-result=zeros(4,5)
+#Released under MIT License 
+#Copyright (c) 2022 Miroslav Kurka
 
-for i=1:4
-	for k=1:5
-    if ((mat1(i,k)>=0) || (mat2(i,k)>=0))
-		  result(i,k)=0
-		else
-      result(i,k)= mat1(i,k)+mat2(i,k)
-    endif
-  endfor
+t_i = [2 2.5 3 3.5 4 4.5 5]; # t_i is our x_i value
+y_i = [7.14 6.56 5.98 5.55 5.71 6.01 6.53];
+residues=[];
+#n= length(t_i) 
+n=columns(t_i);
+k= n-1; # degree of the polynomial
+# init matrices
+V=zeros(n,n);
+Y=zeros(n,1);
+
+
+    
+# fill Y matrix
+
+for i=1:n
+   Y(i)=sum(y_i.*(t_i.^(i-1))); # i-1 so we can get t_i^0 = 1, hence the n not k in the for loop range
 endfor
+
+# fill the V matrix
+    
+for i=1:n
+  for j=1:n
+    if i==1 && j==1
+      V(i,j)=k;
+    else
+      V(i,j)=sum(t_i.^(i+j)-2);
+    endif
+    endfor
+endfor
+    
+
+st=7;
+# get each degree polynomial by slicing the V 
+for pos=1:st
+    if st-pos==0, break; end # end for loop since 0x0 doesnt exist, and indexing is nightmare in matlab
+    V=V(1:st-pos,1:st-pos); # reverse slicing, we reduce matrix from to 6x6 to 5x5 ...
+    Y=Y(1:st-pos);
+    a_coefficients=V\Y;
+    lst_func=polyval(flip(a_coefficients),y_i);
+    residues(pos)= sum((y_i-lst_func).^2);
+endfor
+# flip the residues since by slicing we got 6x6 ie k=6 first 
+residues=flip(residues);
+poly_degrees=(1:k);
+semilogy(poly_degrees,residues)
+grid on
+hold on
+
+
+# task b) compare with polyfit 
+
+matlab_residues=[];
+
+for i=1:6
+    a_matlab_coefficients=polyfit(t_i,y_i,i);
+    mat_func=polyval(a_matlab_coefficients,y_i);
+    matlab_residues(i)=sum((y_i-mat_func).^2);
+endfor
+
+semilogy(poly_degrees, matlab_residues,"red")
+legend("Our Method","MATLAB polyfit")
+xlabel("Degrees of polynomial")
+ylabel("Residue")
+hold off
+         
