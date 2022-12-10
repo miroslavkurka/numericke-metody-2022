9415302113fafc5a4943aff53d5b23982d7784f1
Miro Kurka <59745102+miroslavkurka@users.noreply.github.com>
2022-12-10 18:05:33 +0100

    Update fourth_task.m

--- code/fourth_task.m
+++ code/fourth_task.m
@@ -7,7 +7,7 @@ f(x)= x/(4+x^2);
 my_func=matlabFunction(f(x)); # cast symbolic to matlab func 
 upper_int=5;
 lower_int=0;
-delta=10^-6;
+epsilon=10^-6;
 number_of_intervals=20; # must be 2*n due to simpson method 
 
 h=(upper_int-lower_int)/number_of_intervals;
@@ -21,7 +21,7 @@ I_simpson_previous=0;
 I_trapezoid_previous=0;
 
 
-while abs(I_trapezoid-I_trapezoid_previous) > delta && abs(I_simpson-I_simpson_previous) > delta
+while abs(I_trapezoid-I_trapezoid_previous) > epsilon && abs(I_simpson-I_simpson_previous) > epsilon
   I_simpson_previous=I_simpson;
   I_trapezoid_previous=I_trapezoid;
   number_of_intervals=number_of_intervals*2;
@@ -35,6 +35,7 @@ endwhile
 
 I_simpson
 I_trapezoid
+number_of_intervals
 matlab_trapezoid=trapz(intervals,f_i)
 matlab_simpson=quadv(my_func,0,5)
 analytical_solution=(1/2 * log(4+5**2))-(1/2 * log(4))
